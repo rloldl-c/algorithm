22a7c7bf1e133a982702104ce9b6fee885920aeb
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-05 13:25:22 +0900

    [Bronze III] Title: 최소, 최대, Time: 508 ms, Memory: 149932 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.py
@@ -0,0 +1,12 @@
+n = int(input())
+arr = list(map(int, input().split()))
+max = arr[0]
+min = arr[0]
+
+for i in range(len(arr)):
+    if arr[i] > max:
+        max = arr[i]
+    elif arr[i] < min:
+        min = arr[i]
+
+print(min, max)
