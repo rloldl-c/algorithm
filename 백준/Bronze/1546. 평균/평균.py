d48d5269d687f60fc7608fb89bfcf1040b4a2f3f
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-05 17:27:40 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.py
@@ -0,0 +1,16 @@
+test = int(input())
+arr = list(map(int, input().split()))
+max = 0
+sum = 0
+
+for i in range(test):
+    if max < arr[i]:
+        max = arr[i]
+
+for i in range(test):
+    arr[i] = arr[i] / max * 100
+
+for i in range(test):
+    sum += arr[i]
+
+print(sum/test)
