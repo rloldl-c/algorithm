5b4e074ea463c47b8dc7ab062a98bbad48a97936
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-10 15:51:59 +0900

    [Silver V] Title: 소수 찾기, Time: 48 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,21 @@
+import sys
+
+test = int(sys.stdin.readline())
+num = list(map(int, sys.stdin.readline().split()))
+cnt = 0
+
+for i in range(test):
+    isPrime = True
+    if num[i] == 1:
+        isPrime - False
+        continue
+
+    for k in range(2, num[i]):
+        if num[i] % k == 0:
+            isPrime = False
+            continue
+
+    if isPrime == True:
+        cnt += 1
+
+print(cnt)
