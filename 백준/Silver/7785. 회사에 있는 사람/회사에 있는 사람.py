9299b06204a931c476a56abfb62bdb16360f3f18
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-19 14:06:45 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4032 ms, Memory: 47684 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,16 @@
+test = int(input())
+staff = {}
+company = []
+
+for i in range(test):
+    name, state = input().split()
+    staff[name] = state
+
+for n in staff:
+    if staff[n] == "enter":
+        company.append(n)
+
+company.sort(reverse=True)
+
+for n in company:
+    print(n)
