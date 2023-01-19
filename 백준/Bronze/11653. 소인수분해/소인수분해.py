3514232abdabc0ea038f2578d187aea0d85ac46a
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-19 16:45:04 +0900

    [Bronze I] Title: 소인수분해, Time: 1428 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,11 +1,14 @@
 import sys
 
-n = int(sys.stdin.readline())
+num = int(sys.stdin.readline().strip())
+cnt = 2
 
-if n == 1:
-    print("")
+if num == 1:
+    print()
 else:
-    for i in range(2, n+1):
-        while(n % i == 0):
-            print(i)
-            n /= i
+    while num != 1:
+        if num % cnt == 0:
+            num //= cnt
+            print(cnt)
+        else:
+            cnt += 1
