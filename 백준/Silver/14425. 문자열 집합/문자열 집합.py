5f0c64173db6c443286761e30d9aecaa6fb6bf78
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-27 14:51:42 +0900

    [Silver III] Title: 문자열 집합, Time: 4684 ms, Memory: 36120 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int, input().split())
+
+s_list = []
+test_list = []
+cnt = 0
+
+for _ in range(n):
+    s_list.append(input().strip())
+
+for _ in range(m):
+    s = input().strip()
+    
+    if s in s_list:
+        cnt += 1
+
+print(cnt)
