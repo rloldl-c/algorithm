f4b4cb74967a7ff7c094bb288ac71aa73f4c841d
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-30 17:35:38 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 34100 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,34 +1,34 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
-
 test = int(input())
 
-for i in range(test):
-    que = deque()
+for _ in range(test):
     num, idx = map(int, input().split())
     numbers = list(map(int, input().split()))
-    
-    for number in numbers:
-        que.append(number)
-
+    que = deque()
     cnt = 0
 
-    if num == 1:
-        print(1)
-    else:
-        while True:
-            top = max(que)
-            tmp = que.popleft()
-            idx -= 1
+    for n in numbers:
+        que.append(n)
 
-            if top == tmp:
-                cnt += 1
-                if idx < 0:
-                    print(cnt)
-                    break
-            else:
-                que.append(tmp)
-                if idx < 0:
-                    idx = len(que) - 1
+    while True:
+        max_ = max(que)
+        # que의 맨 앞 요소 확인
+        tmp = que.popleft()
+        idx -= 1
+
+        # tmp가 최대값이고
+        if tmp == max_:
+            cnt += 1
+            # idx가 0보다 작으면 입력으로 주어진 문서이므로 출력
+            # (if문 들어오기 전에 -1을 해줘서 맨 앞에 요소 인덱스는 0보다 작아짐)
+            if idx < 0:
+                print(cnt)
+                break
+        # tmp가 최대값이 아니면 맨 뒤에 삽입
+        else:
+            que.append(tmp)
+            # idx가 0보다 작아지면 que 맨 마지막 인덱스로 바꿔주기
+            if idx < 0:
+                idx = len(que) -1
