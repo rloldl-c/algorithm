ba7752b56c39b6cc90b307c04c715416759a2031
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-01-30 18:08:55 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -1,17 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-string = ["" for i in range(5)]
-max_len = 0
-res = ""
+string = []
 
-for i in range(5):
-    string[i] = input().strip()
-    max_len = max(max_len, len(string[i]))
+max_len = 0
+
+for _ in range(5):
+    s = input().strip()
+    string.append(s)
+    max_len = max(max_len, len(s))
 
 for i in range(max_len):
     for j in range(5):
-        if i < len(string[j]):
-            res += string[j][i]
-
-print(res)
+        if i <len(string[j]):
+            print(string[j][i], end="")
