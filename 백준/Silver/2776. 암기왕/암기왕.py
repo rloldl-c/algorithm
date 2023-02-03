33a0d04e5ce095e33fee2400c1bdf897fbe8c4d2
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-02-03 11:58:33 +0900

    [Silver IV] Title: 암기왕, Time: 1564 ms, Memory: 202200 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -2,33 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-def search(t, arr):
-    s = 0
-    e = len(arr)-1
-
-    while s <= e:
-        m = (s+e) // 2
-        if t == arr[m]:
-            return 1
-        elif t > arr[m]:
-            s = m + 1
-        else:
-            e = m - 1
-
 test = int(input())
 
 for _ in range(test):
     note1 = int(input())
     note1_num = list(map(int, input().split()))
-    note1_num = sorted(note1_num)
     note2 = int(input())
     note2_num = list(map(int, input().split()))
+    
+    num_dict = {}
+    for n in note1_num:
+        num_dict[n] = 1
 
-    for i in note2_num:
-        s = 0
-        e = note1 - 1
-
-        if (search(i, note1_num)) == 1:
-            print(1)
-        else:
-            print(0)
+    for n in note2_num:
+        print(num_dict.get(n, 0))
