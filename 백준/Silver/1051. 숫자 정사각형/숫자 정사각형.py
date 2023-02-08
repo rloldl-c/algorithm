0116ecbdb2828d057f72d13372f1df3a16611b94
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-02-08 15:14:12 +0900

    [Silver IV] Title: 숫자 정사각형, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = []
+breaker = False
 
 for _ in range(n):
     arr.append(list(input().strip()))
@@ -13,12 +14,16 @@ max_ = 1
 min_ = min(n, m)
 
 # 정사각형의 길이
-for i in range(1, min_):
+for i in range(min_, -1, -1):
     # 정사각형을 만들 때 리스트 범위에서 벗어나지 않도록 입력받은 수에서 i만큼 빼주기
     for j in range(n - i):
         for k in range(m - i):
             # 4개의 꼭짓점이 같은 수면 이전에 찾은 넓이와 비교해서 최대값으로 갱신
             if arr[j][k] == arr[j+i][k] == arr[j][k+i] == arr[j+i][k+i]:
-                max_ = max(max_, ((i+1)*(i+1)))
-            
-print(max_)
+                print((i+1)**2)
+                breaker = True
+                break
+        if breaker:
+            break
+    if breaker:
+        break
