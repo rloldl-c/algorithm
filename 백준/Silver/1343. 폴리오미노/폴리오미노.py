326a8d0357f0936eefa2a92d196c6a827204c917
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-02-26 20:12:41 +0900

    [Silver V] Title: 폴리오미노, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -2,41 +2,11 @@ import sys
 input = sys.stdin.readline
 
 board = input().strip()
-res = ''
-cnt = 0
 
-for i in range(len(board)):
-    if board[i] == 'X':
-        cnt += 1
-    elif board[i] == '.':
-        while cnt != 0:
-            if cnt >= 4:
-                res += 'AAAA'
-                cnt -= 4
-            elif cnt >= 2:
-                res += 'BB'
-                cnt -= 2
-            else:
-                res = '-1'
-                break
+board = board.replace('XXXX', 'AAAA')
+board = board.replace('XX', 'BB')
 
-        res += '.'
-        cnt = 0
-
-    if '-1' in res:
-        res = '-1'
-        cnt = 0
-        break
-
-while cnt != 0:
-    if cnt >= 4:
-        res += 'AAAA'
-        cnt -= 4
-    elif cnt >= 2:
-        res += 'BB'
-        cnt -= 2
-    else:
-        res = '-1'
-        break
-
-print(res)
+if 'X' in board:
+    print(-1)
+else:
+    print(board)
