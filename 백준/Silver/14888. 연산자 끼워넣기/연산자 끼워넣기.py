edd76f73062c0561ac8986ca6cb6fc616e42ed3d
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-03-18 21:57:37 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+def exp(cnt):
+    if cnt == n-1:
+        tmp = number[0]
+        for i in range(n-1):
+            if order[i] == 0:
+                tmp += number[i+1]
+            elif order[i] == 1:
+                tmp -= number[i+1]
+            elif order[i] == 2:
+                tmp *= number[i+1]
+            else:
+                tmp = int(tmp/number[i+1])
+
+        res.append(tmp)
+        return
+
+    for i in range(4):
+        if oper[i]:
+            order.append(i)
+            oper[i] -= 1
+            exp(cnt+1)
+            oper[i] += 1
+            order.pop()
+            
+
+n = int(input())
+number = list(map(int, input().split()))
+oper = list(map(int, input().split()))
+order = []
+res = []
+
+exp(0)
+
+if len(res) == 1:
+    print(*res, *res, sep="\n")
+else:
+    print(max(res))
+    print(min(res))
