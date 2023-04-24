64d9826958805a241e883b9f1b8df5c19b32dedc
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-04-24 17:50:23 +0900

    [Silver I] Title: 괄호의 값, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/2504. 괄호의 값/괄호의 값.py
+++ 백준/Silver/2504. 괄호의 값/괄호의 값.py
@@ -1,6 +1,12 @@
 import sys
 input = sys.stdin.readline
 
+'''
+str: (  (  )  [  [   ]   ]   )   (   [   ]   )
+tmp: 2  4  2  6  18  6   2   1   2   6   2   1
+ans: 0  0  4  4  4   22  22  22  22  22  28  28
+'''
+
 string = list(input().strip())
 stack = []
 tmp = 1
@@ -17,13 +23,16 @@ for i in range(len(string)):
     if string[i] == ')':
         if stack:
             k = stack.pop()
+            # 짝이 맞지 않으면 반복문 탈출
             if k != '(':
                 ans = 0
                 break
             else:
+                # 짝이 맞더라도 직전에 괄호값과 일치할 때만 결과에 더해줘야 함
                 if string[i-1] == '(':
                     ans += tmp
                 tmp //= 2
+        # 스택이 비어있으면 올바르지 않은 괄호임 > 반복문 탈출
         else:
             ans = 0
             break
