e9328b0e6fe519e0f3ed6ea7cca07fa19a063fcf
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-04-26 21:16:37 +0900

    [Silver II] Title: 에디터, Time: 348 ms, Memory: 41948 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -1,29 +1,31 @@
 import sys
 from collections import deque
+
 input = sys.stdin.readline
 
-left = deque(list(input().strip()))
-right = deque()
-n = int(input())
+n = input().strip()
+m = int(input())
+left = deque(list(n))
+right = deque([])
 
-for _ in range(n):
-    cmd = input().split()
-
-    if cmd[0] == "L":
+for _ in range(m):
+    cmd = list(input().split())
+    
+    if cmd[0] == 'L':
         if not left:
             continue
         else:
             tmp = left.pop()
             right.appendleft(tmp)
-        
-    elif cmd[0] == "D":
+
+    elif cmd[0] == 'D':
         if not right:
             continue
         else:
             tmp = right.popleft()
             left.append(tmp)
 
-    elif cmd[0] == "B":
+    elif cmd[0] == 'B':
         if not left:
             continue
         else:
@@ -32,4 +34,5 @@ for _ in range(n):
     else:
         left.append(cmd[1])
 
-print(*left, *right, sep="")
+
+print(''.join(left) + ''.join(right))
