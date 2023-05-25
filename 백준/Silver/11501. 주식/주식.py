c69a78a661a27d89694de0e64c422979a701e582
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-05-25 18:55:01 +0900

    [Silver II] Title: 주식, Time: 2356 ms, Memory: 173228 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,18 +1,22 @@
 import sys
 input = sys.stdin.readline
 
-test = int(input())
+def main():
+    t = int(input())
+    for _ in range(t):
+        day = int(input())
+        cost = list(map(int, input().split()))
+        max_cost = cost[-1]
+        ans = 0
 
-for _ in range(test):
-    day = int(input())
-    cost = list(map(int, input().split()))
-    max_cost = cost[-1]
-    buy = ans = cnt = 0
+        for i in reversed(range(day-1)):
+            if max_cost > cost[i]:
+                ans += max_cost - cost[i]
+            else:
+                max_cost = cost[i]
 
-    for i in reversed(range(day-1)):
-        if max_cost > cost[i]:
-            ans += max_cost - cost[i]
-        else:
-            max_cost = cost[i]
+        print(ans)
 
-    print(ans)
+
+if __name__ == '__main__':
+    main()
