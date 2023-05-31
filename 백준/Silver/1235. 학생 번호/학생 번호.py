50e21379079a406dddef4afda9948b2d63f6e983
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-05-31 23:22:15 +0900

    [Silver IV] Title: 학생 번호, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.py
+++ 백준/Silver/1235. 학생 번호/학생 번호.py
@@ -1,27 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-def is_same(num, idx, now):
-    flag = False
-    for i in range(n):
-        if i != int(now) and num == stu[i][:idx+1]:
-            flag = True
-    
-    return flag
-    
-
 n = int(input())
 stu = [input().strip()[::-1] for _ in range(n)]
 stu_len = len(stu[0])
+idx = 0
 
-for i in range(stu_len):
-    flag = True
-    for j in range(n):
-        if is_same(stu[j][:i+1], i, j):
-            flag = False
+while True:
+    tmp = {}
+    idx += 1
+
+    for num in stu:
+        if num[:idx] in tmp:
+            tmp[num[:idx]] += 1
         else:
-            continue
-    
-    if flag:
-        print(i+1)
+            tmp[num[:idx]] = 1
+
+    if len(tmp) == n:
+        print(idx)
         break
