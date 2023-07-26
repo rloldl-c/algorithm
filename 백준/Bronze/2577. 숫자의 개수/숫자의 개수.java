bc2b02de1bb1a4740b3ad434fb94664ca605c52a
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-07-26 21:40:03 +0900

    [Bronze II] Title: 숫자의 개수, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output = new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int a = Integer.parseInt(input.readLine());
+		int b = Integer.parseInt(input.readLine());
+		int c = Integer.parseInt(input.readLine());
+		int num = a * b * c;
+		int[] count = new int[10];
+		
+		while(num / 10 > 0) {
+			int tmp = num % 10;
+			num /= 10;
+			count[tmp]++;
+		}
+		
+		count[num] ++;
+		
+		for(int n: count) {
+			output.append(n).append("\n");
+		}
+		
+		System.out.println(output);
+	}
+}
