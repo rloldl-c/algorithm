142bff4e63f4783654712986eb9bde4c274c45bf
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-07-29 14:02:59 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder output =  new StringBuilder();
+	static StringTokenizer tokens;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		int N = Integer.parseInt(input.readLine());
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < i; j++) {
+				output.append(" ");
+			}
+			
+			for(int j = 0; j < N - i; j++) {
+				output.append("*");
+			}
+			output.append("\n");
+		}
+		
+		System.out.println(output);
+	}
+}
