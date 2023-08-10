825df824fedd42d2a7cf6ded1d6912ee69e7ae01
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-08-10 21:41:39 +0900

    [Gold V] Title: 토마토, Time: 612 ms, Memory: 106712 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -8,23 +8,28 @@
 boxes[h][r][c]에 입력으로 들어오는 토마토 상태 모두 저장
 3차원 배열을 탐색하면서 익은 토마토를 만나면 bfs 탐색 시작
 6방향으로 탐색하면서 주위에 익지 않은 토마토가 있는지 확인
-하루에 익힐 수 있는 토마토 양 제한하기 -> 이전 턴에서 저장한 토마토 개수
 모든 탐색이 끝나면 익지 않은 토마토가 있는지 확인하고
 안 익은 토마토가 있다면 -1 출력
+총 며칠이 걸렸는지를 체크해야 하므로 토마토 좌표를 저장할 때 현재가 며칠 째인지도 함께 저장해서
+큐에서 뺐을 때 이전 날짜에 1을 더해가면서 체크
 */
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
-	static int N, M, H, cnt, days, boxes[][][], dh[], dr[], dc[];
-	static boolean visited[][][];
+	static int N, M, H, cnt, day, boxes[][][], dh[], dr[], dc[];
 	static Deque<Tomato> que;
 
 	public static void main(String[] args) throws IOException {
@@ -36,7 +41,6 @@ public class Main {
 		dr = new int[] {0, 0, 1, -1, 0, 0};
 		dc = new int[] {0, 0, 0, 0, 1, -1};
 		boxes = new int[H][N][M];
-		visited = new boolean[H][N][M];
 		que = new ArrayDeque<>();
 		
 		for(int h = 0; h < H; h++) {
@@ -46,7 +50,7 @@ public class Main {
 					boxes[h][r][c] = Integer.parseInt(tokens.nextToken());
 					
 					if(boxes[h][r][c] == 1) {
-						que.offer(new Tomato(h, r, c));
+						que.offer(new Tomato(h, r, c, 0));
 					}
 				}
 			}
@@ -54,32 +58,27 @@ public class Main {
 		
 		bfs();
 		
-		System.out.println(days);
+		System.out.println(day);
 		
 	}
 
 	static void bfs() {
-		days = 0;
 				
-		while(!que.isEmpty()) {
-			cnt = que.size();
-			days++;
+		while(!que.isEmpty()) {		
+			Tomato t = que.poll();
+			int h = t.h;
+			int r = t.r;
+			int c = t.c;
+			day = t.day;
 			
-			for(int i = 0; i < cnt; i++) {				
-				Tomato t = que.poll();
-				int h = t.h;
-				int r = t.r;
-				int c = t.c;
+			for(int d = 0; d < 6; d++) {
+				int nh = h + dh[d];
+				int nr = r + dr[d];
+				int nc = c + dc[d];
 				
-				for(int d = 0; d < 6; d++) {
-					int nh = h + dh[d];
-					int nr = r + dr[d];
-					int nc = c + dc[d];
-					
-					if(isIn(nh, nr, nc) && boxes[nh][nr][nc] == 0) {
-						boxes[nh][nr][nc] = 1;
-						que.offer(new Tomato(nh, nr, nc));
-					}
+				if(isIn(nh, nr, nc) && boxes[nh][nr][nc] == 0) {
+					boxes[nh][nr][nc] = 1;
+					que.offer(new Tomato(nh, nr, nc, day+1));
 				}
 			}
 		}
@@ -88,14 +87,13 @@ public class Main {
 			for(int r = 0; r < N; r++) {
 				for(int c = 0; c < M; c++) {
 					if(boxes[h][r][c] == 0) {
-						days = -1;
+						day = -1;
 						return;
 					}
 				}
 			}
 		}
 		
-		days--;
 		return;
 	}
 	
@@ -108,10 +106,12 @@ class Tomato {
 	int h;
 	int r;
 	int c;
+	int day;
 	
-	Tomato(int h, int r, int c) {
+	Tomato(int h, int r, int c, int day) {
 		this.h = h;
 		this.r = r;
 		this.c = c;
+		this.day = day;
 	}
 }
