c6a7403bf0c7ebf0c0e9791b68147392491ec038
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-08-17 20:35:26 +0900

    [D3] Title: 상호의 배틀필드, Time: 123 ms, Memory: 20,880 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,3 +1,18 @@
+/**
+* @author 은비
+* @since 2023-08-17
+* @see 
+* @performance 
+* @category #구현
+* @note 
+* 문제 조건대로만 구현하면 됨
+* 동작이 UDLR일 때는 방향만 다를 뿐 행동은 같으니 1차로 S일 때와 아닐 때로 나누기
+* S는 범위를 벗어나기 전까지 반복해야하니 while로 작성
+* -> 폭탄이 움직일 뿐 차가 움직이는 것은 아님! 임시 변수로 폭탄 위치를 나타내자
+* UDLR인 경우 차의 방향은 범위를 벗어나도 바뀌어야 하므로 d와 현재 위치에서 모양을 바꿔줘야 함
+* 그리고 현재 이동에서 한 칸 움직일 수 있으면 이동
+* -> 이때 지나온 길은 .으로 바꿔줘야 함
+*/
 import java.io.*;
 import java.util.*;
 
@@ -75,21 +90,25 @@ public class Solution {
 						switch (s.charAt(i)) {
 						case 'U':
 							d = 0;
+							map[nr][nc] = '^';
 							move(nr, nc);
 							break;
 							
 						case 'D':
 							d = 1;
+							map[nr][nc] = 'v';
 							move(nr, nc);
 							break;
 							
 						case 'L':
 							d = 2;
+							map[nr][nc] = '<';
 							move(nr, nc);
 							break;
 							
 						case 'R':
 							d = 3;
+							map[nr][nc] = '>';
 							move(nr, nc);
 							break;
 						}
@@ -119,24 +138,6 @@ public class Solution {
 		int tmpR = r + deltas[d][0];
 		int tmpC = c + deltas[d][1];
 
-		switch (d) {
-		case 0:
-			map[nr][nc] = '^';
-			break;
-			
-		case 1:
-			map[nr][nc] = 'v';
-			break;
-			
-		case 2:
-			map[nr][nc] = '<';
-			break;
-			
-		case 3:
-			map[nr][nc] = '>';
-			break;
-		}
-		
 		if(!isIn(tmpR, tmpC) || map[tmpR][tmpC] != '.') {
 			return;
 		}
