5c702d808bf5a2818b28ce35fbe8edccea24be24
eunbi <113498957+rloldl-c@users.noreply.github.com>
2023-10-10 22:40:42 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 304 ms, Memory: 27896 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.java
@@ -1,165 +1,158 @@
-/**
-* @author 은비
-* @since 2023-08-23
-* @see 
-* @performance
-* @category #구현
-* @note 
-* 미세먼지는 한 번에 확산 -> 확산되는 총합을 다른 배열에서 계산한 후 원본 배열에 반영
-*/
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder output = new StringBuilder();
 	static StringTokenizer tokens;
 	static int R, C, T;
-	static int[][] map, topDeltas, bottomDeltas;
-	static Pos[] air;
+	static int[][] map, deltas;
+	static ArrayList<Pos> air;
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
+		deltas = new int[][] {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+		air = new ArrayList<>();
 		tokens = new StringTokenizer(input.readLine());
 		R = Integer.parseInt(tokens.nextToken());
 		C = Integer.parseInt(tokens.nextToken());
 		T = Integer.parseInt(tokens.nextToken());
-		bottomDeltas = new int[][] {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
-		topDeltas = new int[][] {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-		map = new int[R][C];
-		air = new Pos[2];
 		
-		for(int r = 0, idx = 0; r < R; r++) {
+		map = new int[R][C];
+		for(int r = 0; r < R; r++) {
 			tokens = new StringTokenizer(input.readLine());
 			for(int c = 0; c < C; c++) {
 				map[r][c] = Integer.parseInt(tokens.nextToken());
+				
 				if(map[r][c] == -1) {
-					air[idx++] = new Pos(r, c);
+					air.add(new Pos(r, c));
 				}
 			}
 		}
-
-		for(int t = 0; t < T; t++) {
+		
+		for(int t = 0; t < T; t++) {			
 			spreadDust();
-			moveDust();
+			topAir();
+			bottomAir();
 		}
 		
-		int ans = 0;
+		countDust();
+		
+	}
+	
+	static void countDust() {
+		int cnt = 0;
+		
 		for(int r = 0; r < R; r++) {
 			for(int c = 0; c < C; c++) {
 				if(map[r][c] > 0) {
-					ans += map[r][c];
+					cnt += map[r][c];
 				}
 			}
 		}
 		
-		System.out.println(ans);
+		System.out.println(cnt);
+	}
+	
+	// 위쪽 공기청정기
+	static void topAir() {
+		int top = map[0][0];
+		int right = map[0][C-1];
+		int bottom = map[air.get(0).r][C-1];
+		int left = 0;
+		
+		// 상
+		for(int c = 0; c < C-1; c++) {
+			map[0][c] = map[0][c+1];
+		}
+		map[0][C-1] = right;
+		
+		// 좌
+		for(int r = air.get(0).r-1; r > 0; r--) {
+			map[r][0] = map[r-1][0];
+		}
+		map[1][0] = top;
+		
+		// 하
+		for(int c = C-1; c > 0; c--) {
+			map[air.get(0).r][c] = map[air.get(0).r][c-1];
+		}
+		map[air.get(0).r][1] = left; 
+		
+		// 우
+		for(int r = 0; r < air.get(0).r; r++) {
+			map[r][C-1] = map[r+1][C-1];
+		}
+		map[air.get(0).r-1][C-1] = bottom;
+	}
+	
+	// 아래쪽 공기청정기
+	static void bottomAir() {
+		int top = map[air.get(1).r][C-1];
+		int right = map[R-2][C-1];
+		int bottom = map[R-1][1];
+		int left = 0;
+		
+		// 상
+		for(int c = C-1; c > 0; c--) {
+			map[air.get(1).r][c] = map[air.get(1).r][c-1];
+		}
+		map[air.get(1).r][1] = left;
+		
+		// 좌
+		for(int r = air.get(1).r + 1; r < R-1; r++) {
+			map[r][0] = map[r+1][0];
+		}
+		map[R-1][0] = bottom;
+		
+		// 하
+		for(int c = 0; c < C-1; c++) {
+			map[R-1][c] = map[R-1][c+1];
+		}
+		map[R-1][C-1] = right;
+		
+//		// 우
+		for(int r = R-1; r > air.get(1).r; r--) {
+			map[r][C-1] = map[r-1][C-1];
+		}
+		map[air.get(1).r+1][C-1] = top;
 		
 	}
 	
 	static void spreadDust() {
-		int[][] tmp = new int[R][C];
+		int[][] dust = new int[R][C];
 		
 		for(int r = 0; r < R; r++) {
 			for(int c = 0; c < C; c++) {
-				if(map[r][c] > 0) {
-					int cnt = 0; // 확산된 방향의 개수
-					for(int d = 0; d < topDeltas.length; d++) {
-						int nr = r + topDeltas[d][0];
-						int nc = c + topDeltas[d][1];
+				if(map[r][c] != 0) {
+					int cnt = 0; // 확산되는 칸
+					int amount = map[r][c] / 5;
+					
+					for(int d = 0; d < deltas.length; d++) {
+						int nr = r + deltas[d][0];
+						int nc = c + deltas[d][1];
 						
-						if(nr >= 0 && nr < R && nc >= 0 && nc < C && map[nr][nc] != -1) {
-							tmp[nr][nc] += map[r][c] / 5;
-							cnt++;
+						if(!isIn(nr, nc) || map[nr][nc] == -1) {
+							continue;
 						}
+						
+						dust[nr][nc] += amount;
+						cnt++;
 					}
 					
-					tmp[r][c] += -map[r][c] / 5 * cnt;
+					dust[r][c] -= amount * cnt;
 				}
 			}
 		}
-		
 		
 		for(int r = 0; r < R; r++) {
 			for(int c = 0; c < C; c++) {
-				if(map[r][c] != -1) {					
-					map[r][c] += tmp[r][c];
-				}
+				map[r][c] += dust[r][c];
 			}
 		}
-		
-		
 	}
 	
-	static void moveDust() {
-		// 위쪽 바람 순환
-		int r = air[0].r;
-		int c = air[0].c;
-		
-		// 아래
-		int bottomTmp = map[r][C-1];
-		for(int nc = C-1; nc >= c+1; nc--) {
-			if(map[r][nc-1] > 0) {
-				map[r][nc] = map[r][nc-1];
-			} else {
-				map[r][nc] = 0;
-			}
-		}
-		
-		// 오른쪽
-		int rightTmp = map[0][C-1];
-		for(int nr = 0; nr < r; nr++) {
-			map[nr][C-1] = map[nr+1][C-1];
-		}
-		map[r-1][C-1] = bottomTmp;
-		
-		// 위
-		int topTmp = map[0][0];
-		for(int nc = 0; nc < C-1; nc++) {
-			map[0][nc] = map[0][nc+1];
-		}
-		map[0][C-2] = rightTmp;
-		
-		// 왼쪽
-		
-		for(int nr = r-1; nr >= 1; nr--) {
-			map[nr][0] = map[nr-1][0];
-		}
-		map[1][0] = topTmp;
-		
-		// 아래 바람 순환
-		r = air[1].r;
-		c = air[1].c;
-		
-		// 위
-		topTmp = map[r][C-1];
-		for(int nc = C-1; nc >= c+1; nc--) {
-			if(map[r][nc-1] > 0) {				
-				map[r][nc] = map[r][nc-1];
-			} else {
-				map[r][nc] = 0;
-			}
-		}
-		
-		// 오른쪽
-		rightTmp = map[R-1][C-1];
-		for(int nr = R-1; nr >= r+1; nr--) {
-			map[nr][C-1] = map[nr-1][C-1];
-		}
-		map[r+1][C-1] = topTmp;
-		
-		// 아래
-		bottomTmp = map[R-1][0];
-		for(int nc = 0; nc < C-1; nc++) {
-			map[R-1][nc] = map[R-1][nc+1];
-		}
-		map[R-1][C-2] = rightTmp;
-		
-		// 왼쪽
-		for(int nr = r+1; nr < R-1; nr++) {
-			map[nr][0] = map[nr+1][0];
-		}
-		map[R-2][0] = bottomTmp;
+	static boolean isIn(int r, int c) {
+		return r >= 0 && r < R && c >= 0 && c < C;
 	}
 	
 	static class Pos {
@@ -170,6 +163,11 @@ public class Main {
 			super();
 			this.r = r;
 			this.c = c;
+		}
+
+		@Override
+		public String toString() {
+			return "Pos [r=" + r + ", c=" + c + "]";
 		}
 	}
 }
