cbd480a4af1569881f82a7ff08d225ea2883fd2a
eunbi <113498957+rloldl-c@users.noreply.github.com>
2024-07-22 13:15:53 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.32 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    var answer = 0;
+    const dp = Array(n+1).fill(0);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for(let i = 3; i <= n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1000000007 ;
+    }
+    
+    return dp[n];
+}
