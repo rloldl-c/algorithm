63174c848f62af0b4daad53ab1456b3f4c5b104b
eunbi <113498957+rloldl-c@users.noreply.github.com>
2024-07-31 15:28:25 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 14.03 ms, Memory: 40.6 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.js
@@ -9,7 +9,7 @@ function solution(rows, columns, queries) {
         }
     }
     
-    for(const query of queries) {
+    queries.map((query) =>  {
         const [sr, sc, er, ec] = query
         const tmp = arr[sr][ec]
         let min = tmp
@@ -17,37 +17,30 @@ function solution(rows, columns, queries) {
         // 위
         for(let j = ec; j > sc; j--) {
             arr[sr][j] = arr[sr][j-1]
-            // console.log(`${sr}, ${j} = ${sr}, ${j-1}`)
             min = Math.min(min, arr[sr][j-1])
         }
         
-        // console.log("왼쪽")
         // 왼쪽
         for(let i = sr; i < er; i++) {
             arr[i][sc] = arr[i+1][sc]
-            // console.log(`${i}, ${sc} = ${i+1}, ${sc}`)
             min = Math.min(min, arr[i+1][sc])
         }
         
         // 아래
-        // console.log("아래")
         for(let j = sc; j < ec; j++) {
-            // console.log(`${er}, ${j} = ${er}, ${j+1}`)
             arr[er][j] = arr[er][j+1]
             min = Math.min(min, arr[er][j+1])
         }
         
-        // console.log("오른쪽")
         // 오른쪽
         for(let i = er; i > sr; i--) {
-            // console.log(`${i}, ${ec} = ${i-1}, ${ec}`)
             arr[i][ec] = arr[i-1][ec]
             min = Math.min(min, arr[i-1][ec])
         }
         
         arr[sr+1][ec] = tmp
         answer.push(min)
-    }
+    })
     
     return answer;
 }
